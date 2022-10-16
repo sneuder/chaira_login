81d76f38fcdd773acd4c35d4509187bc500e8f13
Esneider Ospina <sneuder8@gmail.com>
2022-10-16 04:01:35 -0500

    Add Element and Component

---
+++ src/components/header/elements.js
@@ -0,0 +1,9 @@
+import styled from '@emotion/styled'
+
+export const HeaderContainer = styled.div`
+
+`
+
+export const Tittle = styled.h1`
+
+`
---
+++ src/components/header/index.jsx
@@ -0,0 +1,11 @@
+import { HeaderContainer, Tittle } from "./elements";
+
+const Header = () => {
+  return (
+    <HeaderContainer>
+      <Tittle></Tittle>
+    </HeaderContainer>
+  );
+};
+
+export default Header;
