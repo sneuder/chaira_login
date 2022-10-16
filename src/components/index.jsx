acf6102bb10392c6a8a4011ee0ebd6ac04e1704f
Esneider Ospina <sneuder8@gmail.com>
2022-10-16 04:01:55 -0500

    Add Card Login Component

---
+++ src/components/index.jsx
@@ -0,0 +1,12 @@
+import Card from './card/index';
+import Header from './header/index';
+
+const CardLogin = () => {
+  return (
+    <Card>
+      <Header />
+    </Card>
+  );
+}
+
+export default CardLogin;
