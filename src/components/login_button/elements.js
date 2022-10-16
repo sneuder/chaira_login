0e54ac19984368fa8948da362a370e222c4c833c
Esneider Ospina <sneuder8@gmail.com>
2022-10-16 17:22:38 -0500

    Add styles

--- src/components/login_button/elements.js
+++ src/components/login_button/elements.js
@@ -7,4 +7,9 @@ export const ContainerButton = styled.div`
 
 export const Button = styled.button`
   width: 100%;
+  padding: 10px;
+  border-radius: 3px;
+  font-weight: 600;
+  color: ${colors.primary};
+  background-color: ${colors.secondary};
 `;
