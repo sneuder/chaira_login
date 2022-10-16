96be83e5efae591b14559e8add65db7f846a8580
Esneider Ospina <sneuder8@gmail.com>
2022-10-16 12:55:14 -0500

    Updated name

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-import App from "./App";
+import App from "./app";
 
 import { Global } from "@emotion/react";
 import { styleGlobal } from "./constants/styles/global";
