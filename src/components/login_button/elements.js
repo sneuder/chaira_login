fa72879db82763f621ddaed5e5294d1607f33a56
Esneider Ospina <sneuder8@gmail.com>
2022-10-16 17:18:41 -0500

    Add login button component

---
+++ src/components/login_button/elements.js
@@ -0,0 +1,10 @@
+import styled from '@emotion/styled'
+import colors from '../../constants/styles/colors';
+
+export const ContainerButton = styled.div`
+  width: 100%;
+`;
+
+export const Button = styled.button`
+  width: 100%;
+`;
---
+++ src/components/login_button/index.jsx
@@ -0,0 +1,11 @@
+import { ContainerButton, Button } from "./elements";
+
+const LoginButton = () => {
+  return (
+    <ContainerButton>
+      <Button>Ingresar</Button>
+    </ContainerButton>
+  );
+};
+
+export default LoginButton;
